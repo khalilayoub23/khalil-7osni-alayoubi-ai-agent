1edc3888a3d2e18532cba4d5749e74ad238168b5
khalil ayoub <khalilayoub23@hotmail.com>
2025-01-16 02:20:02 +0000

    update 1.1

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, jsonify
 from khalil_7osni_alayoubi_super_agent import Khalil7osniAlayoubiSuperAgent, BaseAgent
 
 app = Flask(__name__)
-khalil_7osni_al_ayoubi = Khalil7osniAlayoubiSuperAgent()
+khalil_7osni_alayoubi = Khalil7osniAlayoubiSuperAgent()
 
 # Register agents
 agent1 = BaseAgent("Demand Forecaster")
 agent2 = BaseAgent("Supplier Finder")
-khalil_7osni_al_ayoubi.register_agent(1, agent1)
-khalil_7osni_al_ayoubi.register_agent(2, agent2)
+khalil_7osni_alayoubi.register_agent(1, agent1)
+khalil_7osni_alayoubi.register_agent(2, agent2)
 
 @app.route('/')
 def home():
@@ -17,8 +17,8 @@ def home():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.form['user_input']
-    response = khalil_7osni_al_ayoubi.delegate_task(1, user_input)
+    response = khalil_7osni_alayoubi.delegate_task(1, user_input)
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5002, debug=True)
--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,11 @@
-
-fetch("http://backend:5000/")
-    .then(response => response.json())
-    .then(data => console.log(data.message))
-    .catch(error => console.error("Error:", error));
+$(document).ready(function() {
+    $('#send-button').click(function() {
+        var userInput = $('#user-input').val();
+        $('#responses').append('<div>You: ' + userInput + '</div>');
+        $.post('/chat', { user_input: userInput }, function(data) {
+            $('#responses').append('<div>Agent: ' + data.response + '</div>');
+            $('#user-input').val('');
+            $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
+        });
+    });
+});
